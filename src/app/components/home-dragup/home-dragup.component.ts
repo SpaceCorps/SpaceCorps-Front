48de11ddb8815fbd02d64655d07df8b97ac388f9
rorychatt <pavel.rinne@gmail.com>
2025-03-09 00:28:36 +0100

    info: refactor structure

---
+++ src/app/components/home-dragup/home-dragup.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {HomeCarouselComponent} from "../home-carousel/home-carousel.component";
+
+@Component({
+  selector: 'app-home-dragup',
+    imports: [
+        HomeCarouselComponent
+    ],
+  templateUrl: './home-dragup.component.html',
+  styleUrl: './home-dragup.component.scss'
+})
+export class HomeDragupComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
 import {Component} from '@angular/core';
 import {LoginComponent} from '../login/login.component';
+import {HomeCarouselComponent} from '../components/home-carousel/home-carousel.component';
+import {HomeDragupComponent} from '../components/home-dragup/home-dragup.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    LoginComponent
+    LoginComponent,
+    HomeCarouselComponent,
+    HomeDragupComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
