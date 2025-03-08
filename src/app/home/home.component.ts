c691fac0581b301593b78e79cc99905cc2b84cc1
rorychatt <pavel.rinne@gmail.com>
2025-03-08 22:29:54 +0100

    feat: main page structure

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
+import {LoginComponent} from '../login/login.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    LoginComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
