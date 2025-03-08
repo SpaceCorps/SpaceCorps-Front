377cef49428767c8694f5b0b0b39d3042249ee27
rorychatt <pavel.rinne@gmail.com>
2025-03-08 21:51:48 +0100

    info: remove github timeline component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,10 @@
-import { Component } from '@angular/core';
-import { NavbarComponent } from '../components/navbar/navbar.component';
-import { GithubTimelineComponent } from '../components/github-timeline/github-timeline.component';
+import {Component} from '@angular/core';
 
 @Component({
-    selector: 'app-home',
-    imports: [
-        GithubTimelineComponent
-    ],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
