fd7db86b17c9606654a12607b4e751a4b7fe89c1
Dumacuk <dmitri.belov.db@gmail.com>
2025-03-24 20:35:20 +0200

    feat: create cards for player info

---
+++ src/app/components/player-info-ranking/player-info-ranking.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-info-ranking',
+  imports: [],
+  templateUrl: './player-info-ranking.component.html',
+  styleUrl: './player-info-ranking.component.scss'
+})
+export class PlayerInfoRankingComponent {
+
+}
