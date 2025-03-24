f6e3b9f42221a70eb30f85d138b5d96f97cacfba
rorychatt <pavel.rinne@gmail.com>
2025-03-24 20:05:08 +0100

    feat: read in column names

--- src/app/components/player-info-ranking/player-info-ranking.component.ts
+++ src/app/components/player-info-ranking/player-info-ranking.component.ts
@@ -4,8 +4,32 @@ import { Component } from '@angular/core';
   selector: 'app-player-info-ranking',
   imports: [],
   templateUrl: './player-info-ranking.component.html',
-  styleUrl: './player-info-ranking.component.scss'
+  styleUrl: './player-info-ranking.component.scss',
 })
 export class PlayerInfoRankingComponent {
+  constructor() {}
 
+  playerRanks: PlayerRankDto[] = [];
+  playerRankFields: PlayerRankField[] = ['EXP', 'RP', 'ALD', 'SLD'];
 }
+
+type PlayerRankField = PlayerRankDto[keyof PlayerRankDto]['shortName'];
+
+export type PlayerRankDto = {
+  experience: {
+    shortName: 'EXP';
+    value: number;
+  };
+  rankingPoints: {
+    shortName: 'RP';
+    value: number;
+  };
+  aliensDestroyed: {
+    shortName: 'ALD';
+    value: number;
+  };
+  shipsDestroyed: {
+    shortName: 'SLD';
+    value: number;
+  };
+};
