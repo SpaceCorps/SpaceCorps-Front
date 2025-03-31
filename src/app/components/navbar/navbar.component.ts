85829cc6616089ecabcad71f0ecdac3e32d57ae5
rorychatt <pavel.rinne@gmail.com>
2025-03-31 22:05:48 +0200

    feat: render component on click

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,35 +2,38 @@ import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { AsyncPipe } from '@angular/common';
-import { MainMenuComponent } from "../main-menu/main-menu.component";
+import { MainMenuComponent } from '../main-menu/main-menu.component';
 import { UserCredentialsLoginRequest } from '../../models/auth/UserCredentialsLoginRequest';
+import { GithubTimelineComponent } from '../github-timeline/github-timeline.component';
 
 @Component({
-    selector: 'app-navbar',
-    imports: [
-        RouterLink,
-        AsyncPipe,
-        MainMenuComponent,    ],
-    templateUrl: './navbar.component.html',
-    styleUrl: './navbar.component.scss'
+  selector: 'app-navbar',
+  imports: [RouterLink, AsyncPipe, MainMenuComponent, GithubTimelineComponent],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-
-  authService = inject(AuthService)
+  authService = inject(AuthService);
   authState$ = this.authService.authState$;
+
+  showPatchInfo = false;
 
   logOut() {
     this.authService.logOut();
   }
 
-  toggleLogin ($event: UserCredentialsLoginRequest) {
+  toggleLogin($event: UserCredentialsLoginRequest) {
     this.authService.logIn($event).subscribe({
       next: (response) => {
         this.authService.fetchUserAfterSuccessfulLogin(response);
       },
       error: (err) => {
         throw err;
-      }
-    })
+      },
+    });
+  }
+
+  togglePatchInfo() {
+    this.showPatchInfo = !this.showPatchInfo;
   }
 }
