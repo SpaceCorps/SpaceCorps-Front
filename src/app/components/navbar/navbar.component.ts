4d0a172f505e6ee3529a04da97d90c4ac1ac717b
ArtemKhvorostianyi <ar.khvorostianyi@gmail.com>
2025-04-03 01:37:49 +0300

    feat: creating box with patch info

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,16 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common'; // Import CommonModule here
 import { MainMenuComponent } from '../main-menu/main-menu.component';
-import { UserCredentialsLoginRequest } from '../../models/auth/UserCredentialsLoginRequest';
 import { GithubTimelineComponent } from '../github-timeline/github-timeline.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, AsyncPipe, MainMenuComponent, GithubTimelineComponent],
+  standalone: true,
+  imports: [RouterLink, AsyncPipe, CommonModule, MainMenuComponent, GithubTimelineComponent], 
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss',
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
   authService = inject(AuthService);
@@ -18,26 +18,22 @@ export class NavbarComponent {
 
   showPatchInfo = false;
 
+  @ViewChild('patchInfoContainer', { static: false }) patchInfoContainer!: ElementRef;
+
   logOut() {
     this.authService.logOut();
-  }
-
-  toggleLogin($event: UserCredentialsLoginRequest) {
-    this.authService.logIn($event).subscribe({
-      next: (response) => {
-        this.authService.fetchUserAfterSuccessfulLogin(response);
-      },
-      error: (err) => {
-        throw err;
-      },
-    });
   }
 
   togglePatchInfo() {
     this.showPatchInfo = !this.showPatchInfo;
   }
 
-
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    if (this.patchInfoContainer && !this.patchInfoContainer.nativeElement.contains(event.target)) {
+      this.showPatchInfo = false;
+    }
+  }
   // TODO: remove too many divs (move css classes around)
   // TODO: position absolute for timeline component
   // TODO: edit colors in github-timeline component to use theme colors
