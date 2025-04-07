b7f0bd0fecf85e3b5adfc66d5d7201efaf91d400
ArtemKhvorostianyi <ar.khvorostianyi@gmail.com>
2025-04-07 23:43:23 +0300

    info: format

--- src/app/components/github-timeline/github-timeline.component.spec.ts
+++ src/app/components/github-timeline/github-timeline.component.spec.ts
@@ -1,8 +1,8 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {GithubTimelineComponent} from './github-timeline.component';
-import {provideHttpClient} from '@angular/common/http';
-import {provideHttpClientTesting} from '@angular/common/http/testing';
+import { GithubTimelineComponent } from './github-timeline.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('GithubTimelineComponent', () => {
   let component: GithubTimelineComponent;
@@ -11,12 +11,8 @@ describe('GithubTimelineComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [GithubTimelineComponent],
-      providers: [
-        provideHttpClient(),
-        provideHttpClientTesting()
-      ]
-    })
-      .compileComponents();
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GithubTimelineComponent);
     component = fixture.componentInstance;
