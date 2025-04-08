db3ec0eb591d1bd455f53bd9e1053d295fa666a9
rorychatt <pavel.rinne@gmail.com>
2025-04-08 15:35:20 +0200

    feat: angular form adjustments

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,41 +1,56 @@
 import { Component, output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { NgClass } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgClass, NgIf } from '@angular/common';
 import { UserCredentialsLoginRequest } from '../../models/auth/UserCredentialsLoginRequest';
 
 @Component({
   selector: 'app-login-form',
-  imports: [ReactiveFormsModule, NgClass],
+  imports: [ReactiveFormsModule, NgClass, NgIf],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.scss'
+  styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
   loginForm = new FormGroup({
-    login: new FormControl(''),
-    password: new FormControl(''),
+    login: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
   });
 
   toggleViewEvent = output<void>();
   toggleLoginEvent = output<UserCredentialsLoginRequest>();
+
+  get login() {
+    return this.loginForm.get('login');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
 
   toggleView() {
     this.toggleViewEvent.emit();
   }
 
   handleLogin() {
-    let userCredentialsLoginRequest: UserCredentialsLoginRequest;
-    userCredentialsLoginRequest = {
-      email: this.loginForm.get('login')!.value!,
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const loginValue = this.loginForm.get('login')!.value!;
+    const isEmail = loginValue.includes('@');
+
+    const userCredentialsLoginRequest: UserCredentialsLoginRequest = {
+      [isEmail ? 'email' : 'username']: loginValue,
       password: this.loginForm.get('password')!.value!,
     };
 
-    if (!this.loginForm.get('login')!.value!.includes('@')) {
-      userCredentialsLoginRequest = {
-        username: this.loginForm.get('login')!.value!,
-        password: this.loginForm.get('password')!.value!,
-      };
-    }
     this.toggleLoginEvent.emit(userCredentialsLoginRequest);
   }
 }
-
--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,39 +1,82 @@
 import { Component, output } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgClass } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  ValidatorFn,
+  AbstractControl,
+} from '@angular/forms';
+import { NgClass, NgIf } from '@angular/common';
 import { UserCredentialsCreateRequest } from '../../models/auth/UserCredentialsCreateRequest';
 
 @Component({
-    selector: 'app-register-form',
-    imports: [
-        FormsModule,
-        ReactiveFormsModule,
-        NgClass
-    ],
-    templateUrl: './register-form.component.html',
-    styleUrl: './register-form.component.scss'
+  selector: 'app-register-form',
+  imports: [ReactiveFormsModule, NgClass, NgIf],
+  templateUrl: './register-form.component.html',
+  styleUrl: './register-form.component.scss',
 })
 export class RegisterFormComponent {
-  registerForm = new FormGroup({
-    username: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl('')
-  })
+  passwordMatchValidator: ValidatorFn = (control: AbstractControl) => {
+    const form = control as FormGroup;
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  };
+
+  registerForm = new FormGroup(
+    {
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+        Validators.pattern('^[a-zA-Z0-9_]+$'),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(
+          '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$',
+        ),
+      ]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    },
+    { validators: this.passwordMatchValidator },
+  );
 
   toggleViewEvent = output<void>();
   toggleRegisterEvent = output<UserCredentialsCreateRequest>();
 
-  toggleView () {
+  get username() {
+    return this.registerForm.get('username');
+  }
+  get email() {
+    return this.registerForm.get('email');
+  }
+  get password() {
+    return this.registerForm.get('password');
+  }
+  get confirmPassword() {
+    return this.registerForm.get('confirmPassword');
+  }
+
+  toggleView() {
     this.toggleViewEvent.emit();
   }
 
-  handleRegister () {
+  handleRegister() {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+
     const userCredentialsCreateRequest: UserCredentialsCreateRequest = {
       username: this.registerForm.get('username')!.value!,
       email: this.registerForm.get('email')!.value!,
-      password: this.registerForm.get('password')!.value!
-    }
+      password: this.registerForm.get('password')!.value!,
+    };
+
     this.toggleRegisterEvent.emit(userCredentialsCreateRequest);
   }
 }
