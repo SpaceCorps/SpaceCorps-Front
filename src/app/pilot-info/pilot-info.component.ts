38eba14aea5b485eb8381a09d03390374095c773
rorychatt <pavel.rinne@gmail.com>
2025-04-20 17:09:51 +0200

    feat: remove theme picker from pilot info

--- src/app/pilot-info/pilot-info.component.ts
+++ src/app/pilot-info/pilot-info.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { PlayerInfoGeneralInfoComponent } from '../components/player-info-general-info/player-info-general-info.component';
-import { ThemePickerComponent } from '../components/theme-picker/theme-picker.component';
 
 @Component({
   selector: 'app-pilot-info',
-  imports: [PlayerInfoGeneralInfoComponent, ThemePickerComponent],
+  imports: [PlayerInfoGeneralInfoComponent],
   templateUrl: './pilot-info.component.html',
   styleUrl: './pilot-info.component.scss',
 })
